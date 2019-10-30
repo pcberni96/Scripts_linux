d2a7f18d0351c38cf20da969374dfe3420ce5834
pcberni96 <45403377+pcberni96@users.noreply.github.com>
2019-10-30 17:52:56 -0600

    Update config_dns.sh

--- config_dns.sh
+++ config_dns.sh
@@ -1,67 +1,60 @@
 #!/bin/bash
 cp /etc/named.conf /etc/named.conf.org
 
-sed -i '/options {/ a \    listen-on port 53 {127.0.0.1;};' /etc/named.conf
+sed -i '/listen-on port 53 { 127.0.0.1; };/ c #listen-on port 53 { 127.0.0.1; };' /etc/named.conf
 
-sed -i '/};/ a \     listen-on-v6 port 53 { ::1; };' /etc/named.conf
+sed -i '/listen-on-v6 port 53 { ::1; };/ c #listen-on-v6 port 53 { ::1; };' /etc/named.conf
 
-sed -i '/recursing";/ a \     allow-query { localhost: 192.168.217.0/24 };' /etc/named.conf 
+sed -i '/allow-query { localhost; 192.168.217.0/24 };/c allow-query { localhost: 192.168.217.0/24 };' /etc/named.conf 
 
 cat <<EOF> /etc/named.conf
-//Forward Zone
 zone "server" IN{
 	type master;
 	file "server.db";
 	allow-update { none; };
 	allow-query {any; }
 };
-EOF
 
-cat <<EOF> /etc/named.conf
-//Backward Zone
 zone "217.168.192.in-addr.arpa" IN{
 	type master;
 	file "server.rev";
 	allow-update { none; };
-	allow-query { any;}
-}:
-
-cat <<EOF> /var/named/server.db
-$TTL 86400
-@ IN SOA dns-primary.server admin.server (
-	2019061800 ;Serial
-	3600 ;Refresh
-	1800 ;Retry
-	604800 ;Exire
-	86400 ;Minimun TTL
-)
-
-;Name Server Information
-@ IN NS dns-primary.server
-
-;IP for Name Server
-dns-primary IN A 192.168.217.
-
-;A Record for IP address to Hostname 
-slave IN A 192.168.217.
+};
 EOF
 
-cat <<EOF> /var/named/server.rev
-$TTL 86400
-@ IN SOA dns-primary.server admin.server (
-	2019061800 ;Serial
-	3600 ;Refresh
-	1800 ;Retry
-	604800 ;Exire
-	86400 ;Minimun TTL
+cat <<EOF>> /var/named/server.db
+//Doc Server
+\$TTL 86400
+@ IN SOA slave01.server. root.server. (
+        2019061800 ;Serial
+        3600 ;Refresh
+        1800 ;Retry
+        604800 ;Expire
+        86400 ;Minimun TTL
 )
+@ IN NS slave01.server
 
-;Name Server Information
-@ IN NS dns-primary.server
+@ IN A 192.168.217.129
 
-;Reverse lookup for Name Server
-10 IN PTR dns-primary.server
-
-;PTR Record IP address to Hostname 
-5 IN PTR slave.server
+slave01 IN A 192.168.217.129
 EOF
+
+
+cat <<EOF>> /var/named/server.rev
+//Doc rev
+\$TTL 86400
+@ IN SOA slave01.server. root.server. (
+        2019061800 ;Serial
+        3600 ;Refresh
+        1800 ;Retry
+        604800 ;Expire
+        86400 ;Minimun TTL
+)
+@ IN NS slave01.server.
+
+@ IN PTR server.
+
+slave01 IN A 192.168.217.129
+
+129 IN PTR slave01.server.
+
