e0ec37024c19ef094b40379a349090893c66748d
pcberni96 <45403377+pcberni96@users.noreply.github.com>
2020-07-08 11:24:43 -0500

    Update ansible_deploy_allinone.sh

--- ansible_deploy_allinone.sh
+++ ansible_deploy_allinone.sh
@@ -1,21 +1,35 @@
 #!/bin/bash
 
+echo "Establecer Variables correcta a zona geografica"
+echo "----------------------------------------------"
+export var01=$(locale | grep LC_MESSAGES | cut -c 14-24)
+export var02=$(locale | grep LC_MONETARY | cut -c 14-24)
+export var03=$(locale | grep LC_NUMERIC | cut -c 14-24)
+export var04=$(locale | grep LC_TIME | cut -c 14-24)
+
+echo "Establecer Variables definidas por el usuario"
+echo "----------------------------------------------"
+
+export admin_passwd='Pon_tu_password'
+export pg_passwd='Pon_tu_password'
+export rabbitmq_passwd='Pon_tu_password'
+
+echo "Instalación/Update software requerido "
+echo "----------------------------------------------"
+
 yum install epel-release -y
 yum install ansible vim wget git python3 -y
 
-alternatives --set python /usr/bin/python3
+#alternatives --set python /usr/bin/python3# Alternativo #
+
+echo "Instalación de Red Hat Ansible"
+echo "----------------------------------------------"
 
 mkdir /tmp/tower && cd /tmp/tower
 
 wget https://releases.ansible.com/ansible-tower/setup/ansible-tower-setup-latest.tar.gz
 
 tar xvzf ansible-tower-setup-3.[67].[45]-[123].tar.gz
-
-var01=$(locale | grep LC_MESSAGES | cut -c 14-24)
-var02=$(locale | grep LC_MONETARY | cut -c 14-24)
-var03=$(locale | grep LC_NUMERIC | cut -c 14-24)
-var04=$(locale | grep LC_TIME | cut -c 14-24)
-
 
 sed -i '/lc_messages = 'en_US.UTF-8'/ c $var01' ansible-tower-setup-3.[67].[45]-[123]/roles/postgres/templates/postgresql.conf.j2
 
