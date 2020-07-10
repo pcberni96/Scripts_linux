e85c1200bffdceb0f6b2246d4a25a783aebb4c6c
pcberni96 <45403377+pcberni96@users.noreply.github.com>
2020-07-09 23:10:34 -0500

    Update ansible_deploy_allinone.sh

--- ansible_deploy_allinone.sh
+++ ansible_deploy_allinone.sh
@@ -4,8 +4,8 @@ echo "Establecer Variables correcta a zona geografica"
 echo "----------------------------------------------"
 export var01=$(locale | grep LC_MESSAGES | cut -c 14-24)
 export var02=$(locale | grep LC_MONETARY | cut -c 14-24)
-export var03=$(locale | grep LC_NUMERIC | cut -c 14-24)
-export var04=$(locale | grep LC_TIME | cut -c 14-24)
+export var03=$(locale | grep LC_NUMERIC | cut -c 13-23)
+export var04=$(locale | grep LC_TIME | cut -c 10-20)
 
 echo "Establecer Variables definidas por el usuario"
 echo "----------------------------------------------"
@@ -17,32 +17,44 @@ export rabbitmq_passwd='Pon_tu_password'
 echo "Instalación/Update software requerido "
 echo "----------------------------------------------"
 
-yum install epel-release -y
+# yum install epel-release -y # #Opcional 
 yum install ansible vim wget git python3 -y
 
-#alternatives --set python /usr/bin/python3# Alternativo #
+#alternatives --set python /usr/bin/python3# Alternativo # #Opcional
+
+echo "Descarga de archivo Ansible"
+echo "----------------------------------------------"
+
+mkdir /tmp/tower
+cd /tmp/tower
+echo $(pwd)
+wget https://releases.ansible.com/ansible-tower/setup/ansible-tower-setup-latest.tar.gz
+
+echo "Descomprimiendo archivo .tar"
+echo "------------------------------------------------"
+tar xvf /tmp/tower/$(ls -a /tmp/tower | grep ansible) -C /tmp/tower
+
+echo "Reemplazando Variables Timezones"
+echo "------------------------------------------------"
+
+sed -i '/lc_messages =/ c lc_messages = \'\'$var01'\'\' /tmp/tower/$(ls -a /tmp/tower | grep ansible-tower-setup-[34])/roles/postgres/templates/postgresql.conf.j2
+
+sed -i '/lc_monetary =/ c lc_monetary = \'\'$var02'\'\' /tmp/tower/$(ls -a /tmp/tower | grep ansible-tower-setup-[34])/roles/postgres/templates/postgresql.conf.j2
+
+sed -i '/lc_numeric =/ c lc_numeric = \'\'$var03'\'\' /tmp/tower/$(ls -a /tmp/tower | grep ansible-tower-setup-[34])/roles/postgres/templates/postgresql.conf.j2
+
+sed -i '/lc_time =/ c lc_time = \'\'$var04'\'\' /tmp/tower/$(ls -a /tmp/tower | grep ansible-tower-setup-[34])/roles/postgres/templates/postgresql.conf.j2
+
+echo "Reemplazando Variables Passwords"
+echo "------------------------------------------------"
+
+sed -i '/admin_password=/ c admin_password=\'\'$admin_passwd'\'\' /tmp/tower/$(ls -a /tmp/tower | grep ansible-tower-setup-[34])/inventory
+
+sed -i '/pg_password=/ c pg_password=\'\'$pg_passwd'\'\' /tmp/tower/$(ls -a /tmp/tower | grep ansible-tower-setup-[34])/inventory
+
+sed -i '/rabbitmq_password=/ c rabbitmq_password=\'\'$rabbitmq_passwd'\'\' /tmp/tower/$(ls -a /tmp/tower | grep ansible-tower-setup-[34])/inventory
 
 echo "Instalación de Red Hat Ansible"
 echo "----------------------------------------------"
-
-mkdir /tmp/tower && cd /tmp/tower
-
-wget https://releases.ansible.com/ansible-tower/setup/ansible-tower-setup-latest.tar.gz
-
-tar xvzf ansible-tower-setup-3.[67].[45]-[123].tar.gz
-
-sed -i '/lc_messages = 'en_US.UTF-8'/ c $var01' ansible-tower-setup-3.[67].[45]-[123]/roles/postgres/templates/postgresql.conf.j2
-
-sed -i '/lc_monetary = 'en_US.UTF-8'/ c $var02' ansible-tower-setup-3.[67].[45]-[123]/roles/postgres/templates/postgresql.conf.j2
-
-sed -i '/lc_numeric = 'en_US.UTF-8'/ c $var03' ansible-tower-setup-3.[67].[45]-[123]/roles/postgres/templates/postgresql.conf.j2
-
-sed -i '/lc_time = 'en_US.UTF-8'/ c $var05' ansible-tower-setup-3.[67].[45]-[123]/roles/postgres/templates/postgresql.conf.j2
-
-sed -i '/admin_password=''/ c admin_password='$admin_passwd''ansible-tower-setup-3.[67].[45]-[123]/inventory 
-
-sed -i '/pg_password=''/ c pg_password='$pg_passwd''ansible-tower-setup-3.[67].[45]-[123]/inventory
-
-sed -i '/rabbitmq_password=''/ c rabbitmq_password='$rabbitmq_passwd''ansible-tower-setup-3.[67].[45]-[123]/inventory
-
+cd /tmp/tower
 ./setup.sh
